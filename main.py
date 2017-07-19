7be285552edc2b6aaa5d977bd1639ddc32faa71b
dungdinhanh <dinhanhdung1996@gmail.com>
2017-07-19 17:48:45 +0700

    new

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Word
+import word
 import bst
 import tree_node
 
@@ -18,7 +18,7 @@ def read_file(input_file, bst):
         str_list = line.split(":")
         str_list[0] = str_list[0].strip()
         str_list[1] = str_list[1].strip()
-        word = Word.Word()
+        word = word.Word()
         word.input_word(str_list[0], str_list[1])
         bst.insert_node(word)
 
@@ -34,13 +34,13 @@ while True:
     choice = int(input("Your choice: "))
 
     if choice == 1:
-        new_word = Word.Word()
+        new_word = word.Word()
         new_word.eng = input("Input English word: ")
         new_word.vi = input("Input Vietnamese word: ")
         bst.insert_node(new_word)
 
     if choice == 2:
-        new_word = Word.Word()
+        new_word = word.Word()
         new_word.eng = input("Input key word to delete: ")
         new_word = bst.search_node(new_word)
         if new_word is None:
@@ -50,7 +50,7 @@ while True:
         bst.delete_node(new_word.data)
 
     if choice == 3:
-        new_word = Word.Word()
+        new_word = word.Word()
         new_word.eng = input("Input key word to search: ")
         new_word = bst.search_node(new_word)
         if new_word is None:
